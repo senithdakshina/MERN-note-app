6d56401b3465ffa57c581a4c05c53210c459838a
senith <senithdakshina@gmail.com>
2025-10-23 12:12:32 +0530

    crud created

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,22 @@ import expres from "express"
 const app = expres()
 
 app.get("/api/notes",(req,res)=>{
-    res.send("you got 5 notes");
+    res.status(200).send("you got 5 notes");
 })
+
+app.post("/api/notes",(req,res)=>{
+    res.status(201).json({message:"Post created successfully!!"})
+})
+
+app.put("/api/notes/:id",(req,res)=>{
+    res.status(200).json({message:"Post updated successfully!!"})
+})
+
+app.delete("/api/notes/:id",(req,res) =>{
+    res.status(200).json({message:"Post deleted successfully!!"})
+})
+
+
 
 app.listen(5001, () => {
     console.log('server stated on PORT')
