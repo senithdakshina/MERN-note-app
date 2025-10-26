ab7ecef6503272d3c0f696cb18c8bb6e78c8faa3
senith <senithdakshina@gmail.com>
2025-10-26 18:44:12 +0530

    data base connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
-const connectDb = async () => {
+export const connectDb = async () => {
     try{
-        mongoose.connect("mongodb+srv://senithdakshina_db_user:t7iHtRfRycdQg8EH@cluster0.bcjiz5c.mongodb.net/?appName=Cluster0")
+        await mongoose.connect("mongodb+srv://senithdakshina_db_user:t7iHtRfRycdQg8EH@cluster0.bcjiz5c.mongodb.net/?appName=Cluster0");
         console.log("MogoDB is successfully connected!");
     }catch(error){
         console.error("Error connecting MongoDB",error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
+import { connectDB } from ".config/db.js"
 
 const app = express();
 app.use(express.json());
+
+connectDB();
 
 // Mount routes
 app.use("/api/notes", noteRoutes);
