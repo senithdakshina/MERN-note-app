604ed3587d1396a04533383248f3fe639bf141b5
senith <senithdakshina@gmail.com>
2025-11-02 16:27:59 +0530

    Tailwind CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Route, Router, Routes } from 'react-router'
+import HomePage from './Pages/HomePage'
+import CreatePage from './Pages/CreatePage'
+import NoteDetailPage from './Pages/NoteDetailPage'
+import toast from 'react-hot-toast'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={() =>toast.success("ok!!!!")}></button>
+      <Routes>
+
+        <Route path='/' element = {<HomePage />} />
+        <Route path='/create' element={<CreatePage/>}></Route>
+        <Route path='/note/:id' element={<NoteDetailPage/>}></Route>
+      </Routes>
+    </div>
   )
 }
 
