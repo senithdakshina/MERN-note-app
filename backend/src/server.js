86bc965902bb510e97922bd60dfb62d910ee8311
senith <senithdakshina@gmail.com>
2025-11-02 11:34:40 +0530

    Request limit API

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,27 @@ console.log("Loaded MONGO_URI:", process.env.MONGO_URI ? "✅ Loaded" : "❌ Mis
 import { connectDB } from "./config/db.js"
 import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
+import ratelimmiter from './middleware/reteLimitter.js';
 
 
 const app = express();
 app.use(express.json());
 
 await connectDB();
+
 //middlewhere
 app.use(express.json())
+app.use(ratelimmiter)
+app.use((req,res,next)=> {
+
+  console.log(`Request method is ${req.method} & request URL is ${req.url}`);
+
+  next();
+
+}); 
+//middlewhere
+
+
 // Mount routes
 app.use("/api/notes", noteRoutes);
 
