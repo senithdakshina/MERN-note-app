1b5a426283536ef028ab7fe38ef99cf35bd40fc5
senith <senithdakshina@gmail.com>
2025-11-03 21:15:24 +0530

    fetch the data(notes)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,13 @@ import { Route, Router, Routes } from 'react-router'
 import HomePage from './Pages/HomePage'
 import CreatePage from './Pages/CreatePage'
 import NoteDetailPage from './Pages/NoteDetailPage'
+
 // import toast from 'react-hot-toast'
 
 const App = () => {
   return (
     <div data-theme = "aqua">
-      <div className="p-10 bg-blue-500 text-white text-3xl">
-  Tailwind is working 🎉
-  <button className="btn btn-outline">button</button>
-</div>
+    
 
 
       <Routes>
