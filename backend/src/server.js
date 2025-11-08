ac69eee22e4d224648a72a8f3435edf6365771c9
senith <senithdakshina@gmail.com>
2025-11-08 21:25:52 +0530

    .env port

--- backend/src/server.js
+++ backend/src/server.js
@@ -60,7 +60,7 @@ app.get("*",(req,res) => {
 });
 }
 connectDB().then(() =>{
-const PORT = 5001; 
+const PORT = process.env.PORT ||5001; 
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT}`);
   
